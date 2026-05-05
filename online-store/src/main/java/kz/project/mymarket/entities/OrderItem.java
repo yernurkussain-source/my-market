569f3454ff97a68c32a1e3ed094030fc895222ae
nurbakyt <nurbakyt.sagidulla@narxoz.kz>
2026-05-05 17:03:30 +0500

    add OrderItem entity

---
+++ online-store/src/main/java/kz/project/mymarket/entities/OrderItem.java
@@ -0,0 +1,28 @@
+package kz.project.mymarket.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_items")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    private Double price;
+}
